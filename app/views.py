91ffd3306c7183328ab4a848558c66d212bbd4db
Caius I O <hxvx@hotmail.com>
2025-06-19 00:20:04 -0300

    Aula 18/06/25

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.utils.dateparse import parse_date
 from django.utils.timezone import now
@@ -6,7 +6,7 @@ from django.views import View
 import sys
 import socket
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from app.models import Person, Book
 
@@ -43,7 +43,105 @@ class PeopleView(View):
     def get(self, request):
         people = Person.objects.all()
         data = [{"name": p.name, "age": p.age} for p in people]
-        return JsonResponse({"people":data})
+        return render(request, "app/person_list.html", {"people":people})
+
+def person_list_json(request):                #FBV
+    people = Person.objects.all().values()
+    return JsonResponse(list(people), safe=False)
+
+def person_create(request):                #FBV
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        if name and age:
+            Person.objects.create(
+                name=name,
+                age=age,
+            )
+            return redirect("people")  # apos o cadastro, volta para a listagem
+    return render(request, "app/person_form.html")
+
+def person_update(request, person_id):
+    person = get_object_or_404(Person, id=person_id)
+    if request.method == "POST":
+        person.name = request.POST.get("name")
+        person.age = request.POST.get("age")
+        person.save()
+
+        return redirect("people")
+    return render(request, "app/person_form.html",{"person":person})
+
+def person_delete(request, person_id):
+    person = get_object_or_404(Person, id=person_id)
+    if request.method == "POST":
+        person.delete()
+        return redirect("people")
+    return render (request,"app/person_confirm.html",{"person":person})
+
+#----- x ----- x ----- x ----- x ----- x ----- x ----- x ----- x ----- x ----- x ----- x ----- x -----
+
+class BookListJsonView(View):               #CBV
+    def get(self, request):
+        books = Book.objects.all().values()
+        return JsonResponse(list(books), safe=False)
+
+class BooklistView(ListView):
+    model = Book
+    template_name = "app/book_list.html"
+    context_object_name = "books"
+
+class BookCreateView(View):
+    def get(self,request):
+        return render(request,"app/book_form.html")
+    def post(self,request):
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        pages = request.POST.get("pages")
+        published_date = parse_date(request.POST.get("published_date"))
+
+        # Validação de dados
+        if title and author and pages and published_date:
+            Book.objects.create(
+                title=title,
+                author=author,
+                pages=pages,
+                published_date=published_date
+            )
+            return redirect("book_list")  # apos o cadastro, volta para a listagem
+
+        return render(request, "app/book_form.html")
+
+class BookUpdateView(View):
+    def get(self,request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        return render(request, "app/book_form.html", {"book":book})
+
+    def post(self,request,book_id):
+        book = get_object_or_404(Book, id=book_id)
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.pages = request.POST.get("pages")
+        book.published_date = parse_date(request.POST.get("published_date"))
+        book.save()
+        return redirect("book_list")
+
+class BookGetView(DetailView):
+        model = Book
+        template_name = "app/book_detail.html"
+        context_object_name = "book"
+        pk_url_kwarg = "book_id"
+
+class BookDeleteView(View):
+    def get(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        return render(request, "app/book_confirm.html", {"book":book})
+    def post(self,request,book_id):
+        book = get_object_or_404(Book, id=book_id)
+        book.delete()
+        return redirect("book_list")
+
+
+"""Legacy FBVs --
 
 def book_list_json(request):                #FBV
     books = Book.objects.all().values()
@@ -71,7 +169,26 @@ def book_create(request):                #FBV
             return redirect("books")   #apos o cadastro, volta para a listagem
     return render(request,"app/book_form.html")
 
-# Create your views here.
+def book_update(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.pages = request.POST.get("pages")
+        book.published_date = parse_date(request.POST.get("published_date"))
+        book.save()
+
+        return redirect("books")
+    return render(request, "app/book_form.html",{"book":book})
+
+def book_delete(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect("books")
+    return render (request,"app/book_confirm.html",{"book":book})
+
+"""
 
 # esse arquivo gerencia as funções do app
 # python manage.py runserver  <-- para rodar o server
--- config/urls.py
+++ config/urls.py
@@ -19,16 +19,26 @@ from django.contrib import admin
 from django.urls import path
 
 from app import views
-from app.views import BaseHelloView, ServerInfoView, WelcomeView, PeopleView, book_list, book_list_json, book_create
+from app.views import BaseHelloView, ServerInfoView, WelcomeView, PeopleView, person_create, person_update, person_delete, BooklistView, BookCreateView, BookGetView, \
+    BookUpdateView, BookDeleteView, BookListJsonView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("hello", BaseHelloView.as_view(), name='hello'),
-    path("server-info", ServerInfoView.as_view(), name="server-info" ),
+    path("server-info", ServerInfoView.as_view(), name="server_info" ),
     path("welcome/", WelcomeView.as_view(), name="welcome"),
+
     path("people/", PeopleView.as_view(), name="people"),
-    path("books-json/", book_list_json),
-    path("books/", book_list, name='books'),
-    path("books-add/", book_create),
+    path("people-add/", person_create, name="people_add"),
+    path("people-edit/<int:person_id>", person_update, name='people-edit'),
+    path("person-delete/<int:person_id>", person_delete, name='person-delete'),
+
+    path("book/json/", BookListJsonView.as_view(), name="book_list_json"),
+    path("book/list/", BooklistView.as_view(), name='book_list'),
+    path("book", BookCreateView.as_view(), name='book_add'),
+    path("book/<int:book_id>", BookGetView.as_view(), name='book_detail'),
+    path("book/delete/<int:book_id>", BookDeleteView.as_view(), name='book_delete'),
+    path("book/edit/<int:book_id>", BookUpdateView.as_view(), name="book_update"),
+
 ]
 
