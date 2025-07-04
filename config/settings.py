d8d6137319c9232e6ad0292a8387b55ca0a584ce
Caius I O <hxvx@hotmail.com>
2025-07-04 20:56:57 -0300

    Aula 04/07/25 patch env

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lcfk+qyzrrv5d$3hjdg=-kpz#veb$o_t$7wum!5_is@t56&qkc"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
